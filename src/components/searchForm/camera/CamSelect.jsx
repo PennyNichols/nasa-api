9cefbd5d45e41d359e829330ca25ff47195a0a03
pnichols789 <pnichols789@gmail.com>
2022-12-30 08:54:48 -0600

    -

---
+++ src/components/searchForm/camera/CamSelect.jsx
@@ -0,0 +1,45 @@
+import { FormLabel, ToggleButton, ToggleButtonGroup, Typography, useTheme } from "@mui/material";
+import React, { useContext } from "react";
+import { ImageContext } from "../../../context/ImageContext";
+import EarthInput from "./EarthInput";
+import SolInput from "./SolInput";
+
+const CamSelect = () => {
+	const { handleDate, dateType, dateTypeOptions, date, sol } = useContext(ImageContext);
+
+    const theme = useTheme();
+
+	return (
+		<div className="form-select">
+			<FormLabel id="date-selection">
+                <Typography
+					color='primary'
+					variant='h6'
+				>
+					Select a Date
+				</Typography>
+            </FormLabel>
+			<div className="date-options">
+				<ToggleButtonGroup
+                    className='toggle-group'
+					value={dateType}
+                    color='primary'
+					exclusive
+					onChange={handleDate}
+					aria-label="Mars Rover Date Type Selection"
+				>
+					{dateTypeOptions?.map((option,index)=>(
+						<ToggleButton value={option} key={index} aria-label={option}>
+							{(option=='earth_date') ? 'earth date' : 'sol'}
+						</ToggleButton>
+
+					))
+					}
+				</ToggleButtonGroup>
+				{dateType === "earth_date" ? <EarthInput /> : <SolInput />}
+			</div>
+		</div>
+	);
+};
+
+export default CamSelect;
