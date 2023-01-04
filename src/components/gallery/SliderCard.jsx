b8e4ed3551207ccfc29b93c7f62de16bb1503cac
pnichols789 <pnichols789@gmail.com>
2023-01-04 17:39:55 -0600

    helper message for closing full screen view

--- src/components/gallery/SliderCard.jsx
+++ src/components/gallery/SliderCard.jsx
@@ -1,12 +1,42 @@
-import { Card, CardContent, CardMedia } from "@mui/material";
-import React from "react";
+import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const SliderCard = (props) => {
 	const { img_src, camera, earth_date, rover } = props.image;
 	const { full_name } = camera;
 	const { name } = rover;
+
+	const [message, setMessage] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setMessage(false);
+		}, 1500);
+	}, [message]);
+
 	return (
-		<Card style={{ backgroundColor: "transparent" }}>
+		<Card style={{ position: "relative", backgroundColor: "transparent" }}>
+			{message && (
+				<Box
+					style={{
+						position: "absolute",
+						height: "100vh",
+						width: "100vw",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+				>
+					<Typography
+						color="primary"
+						variant="h3"
+						style={{ backgroundColor: "rgba(0, 0, 0, 0.627", padding: "10px" }}
+					>
+						Click or tap image to close
+					</Typography>
+				</Box>
+			)}
 			<CardMedia
 				component="img"
 				image={img_src}
